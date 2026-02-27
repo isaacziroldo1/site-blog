3a72f20d6c2a5a1772ee9a7ea9470aad8e6ef855
Isaac <seu-email@example.com>
2026-02-27 17:56:31 -0300

    feat: atualizar header com background preto e estilos refinados

---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-black/80 backdrop-blur supports-[backdrop-filter]:bg-black/60">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">Header</div>
+        </header>
+    )
+}
--- src/pages/blog/posts/index.tsx
+++
@@ -1,9 +0,0 @@
-
-//exemplo de rota estática
-export default function PostsPage() {
-    return (
-        <div>
-            <h2>Posts</h2>
-        </div>
-    )
-}
